c34935eb112fa0d227f8e2225b6c79c112224bb7
stumburs <stumburs@inbox.lv>
2023-04-19 00:47:23 +0300

    Basic markov generator complete.

--- main.cpp
+++ main.cpp
@@ -1,27 +1,57 @@
-// https://youtu.be/eGFJ8vugIWA?t=845
-
 #include <iostream>
 #include <vector>
 #include <map>
+#include <random>
+#include <iterator>
 
-std::string text = "the theremin is theirs, ok? yes, it is. this is a theremin.";
+// https://en.wikipedia.org/wiki/Markov_chain
+std::string text = "A Markov chain or Markov process is a stochastic model describing a sequence of possible events in which the probability of each event depends only on the state attained in the previous event.[1]. It is a stochastic process with a series of states that alternate between one another. Probabilities based on the current state of the Markov chain determine these transitions.[2]. Definition of Markov chain in US English by Oxford Dictionaries [3]Informally, this may be thought of as, \"What happens next depends only on the state of affairs now.\" A countably infinite sequence, in which the chain moves state at discrete time steps, gives a discrete-time Markov chain (DTMC). A continuous-time process is called a continuous-time Markov chain (CTMC). It is named after the Russian mathematician Andrey Markov. Markov chains have many applications as statistical models of real-world processes,[4][5][6] such as studying cruise control systems in motor vehicles, queues or lines of customers arriving at an airport, currency exchange rates and animal population dynamics.[7]The maximum probability decision rule can also be used by a Markov chain-based approach to determine the language. Empirical experiments demonstrated that, when compared to the N-gram approach, markov chains could recognize languages faster and with a reduced mistake rate [8]. Markov processes are the basis for general stochastic simulation methods known as Markov chain Monte Carlo, which are used for simulating sampling from complex probability distributions, and have found application in Bayesian statistics, thermodynamics, statistical mechanics, physics, chemistry, economics, finance, signal processing, information theory and speech processing.[7][9][10] The adjectives Markovian and Markov are used to describe something that is related to a Markov process.[11][12][13]";
 int order = 3;
 
-std::map<std::string, int> ngrams;
+std::map<std::string, std::vector<std::string>> ngrams;
+
+template <typename Iter, typename RandomGenerator>
+Iter select_randomly(Iter start, Iter end, RandomGenerator &g)
+{
+    std::uniform_int_distribution<> dis(0, std::distance(start, end) - 1);
+    std::advance(start, dis(g));
+    return start;
+}
+
+template <typename Iter>
+Iter select_randomly(Iter start, Iter end)
+{
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    return select_randomly(start, end, gen);
+}
+
+std::string markov(int text_length = 100)
+{
+    std::string current_gram = text.substr(0, order);
+    std::string result = current_gram;
+    for (int i = 0; i < 100; i++)
+    {
+        auto possibilities = ngrams[current_gram];
+        auto next = *select_randomly(possibilities.begin(), possibilities.end());
+        result += next;
+        current_gram = result.substr(result.length() - order, order);
+    }
+    return result;
+}
 
 int main()
 {
     for (int i = 0; i <= text.length() - order; i++)
     {
-        std::string gram = text.substr(i, 3);
-        if (!ngrams.contains(gram))
-            ngrams.insert({gram, 1});
-        else
-            ngrams[gram]++;
+        std::string gram = text.substr(i, order);
+        if (ngrams.find(gram) == ngrams.end())
+        {
+            ngrams[gram] = std::vector<std::string>();
+        }
+        ngrams[gram].push_back(text.substr(i + order, 1));
     }
 
-    for (const auto &[key, value] : ngrams)
-    {
-        std::cout << "\"" << key << "\" - " << value << "\n";
-    }
+    for (int i = 0; i < 10; i++)
+        std::cout << markov() << "\n";
 }
